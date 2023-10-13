11c3d1ed473a286f7f529f5b65428dabfe002556
Steffi Wong <teff.wong@gmail.com>
2023-10-14 06:41:01 +0800

    .

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,7 +3,7 @@
 
   <div class="d-flex align-items-center justify-content-between">
     <a href="dashboard" class="dasboard-logo d-flex align-items-center logo">
-    <img src="{{ asset('admin-assets/logo.svg') }}" alt="">
+    <img src="{{ asset('admin-assets/media/logo.svg') }}" alt="">
     </a>
     <i class="bi bi-list toggle-sidebar-btn"></i>
   </div><!-- End Logo -->
