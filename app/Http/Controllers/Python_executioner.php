4afb68424be6c9730ac3223ea145be7bf5e7b5c3
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2023-10-18 07:46:21 +0800

    just a note

--- app/Http/Controllers/Python_executioner.php
+++ app/Http/Controllers/Python_executioner.php
@@ -24,29 +24,33 @@ class Python_executioner extends Controller
     }
 
     public function execute_script(){
-        $path = dirname(__FILE__,4);
 
-        if(!is_dir($path.'/core/classes')){
-            mkdir($path.'/core/classes');
-        }
-        if(!is_dir($path.'/core/words')){
-            mkdir($path.'/core/words');
-        }
-        if(!is_dir($path.'/core/intents')){
-            mkdir($path.'/core/intents');
-        }
-        if(!is_dir($path.'/core/models')){
-            mkdir($path.'/core/models');
-        }
+        // rewrite this to take in a intent file name
+        {
+            $path = dirname(__FILE__,4);
 
-        $pyscript = 'C:\\wamp64\\www\\PyBot_university_college_chatbot\\core\\python\\training.py';
-        $python = "C:\\Python_3.6\\python.exe";
+            if(!is_dir($path.'/core/classes')){
+                mkdir($path.'/core/classes');
+            }
+            if(!is_dir($path.'/core/words')){
+                mkdir($path.'/core/words');
+            }
+            if(!is_dir($path.'/core/intents')){
+                mkdir($path.'/core/intents');
+            }
+            if(!is_dir($path.'/core/models')){
+                mkdir($path.'/core/models');
+            }
 
-        $output = 'niuce';
-        $iteration = 1;
-        $model_name = 'pybot_model';
+            $pyscript = 'C:\\wamp64\\www\\PyBot_university_college_chatbot\\core\\python\\training.py';
+            $python = "C:\\Python_3.6\\python.exe";
 
-        return shell_exec("$python $pyscript $iteration $path $model_name");
+            $output = 'niuce';
+            $iteration = 1;
+            $model_name = 'pybot_model';
+
+            return shell_exec("$python $pyscript $iteration $path $model_name");
+        }
        
     }
 
