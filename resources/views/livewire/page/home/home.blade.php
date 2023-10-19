3a002273530e3b064ee737055fd892c71d390246
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-10-20 04:10:07 +0800

    display the chatbot interface and handle user input.

--- resources/views/livewire/page/home/home.blade.php
+++ resources/views/livewire/page/home/home.blade.php
@@ -52,25 +52,37 @@
     <img src="{{ asset('admin-assets/media/icon.png') }}" class="icon" alt="logo">
     <div id="chat-overlay"></div>
 	</div>
-  <div class="chat">
-    <div class="chat-title">
-      <div>
-        <h1>PyBot</h1>
-        <h2>ACTIVE</h2>
+
+
+    <!-- Chatbot Elements -->
+    <div id="chat-circle" class="btn btn-raised">
+      <img src="{{ asset('admin-assets/media/icon.png') }}" class="icon" alt="logo">
+      <div id="chat-overlay"></div>
+    </div>
+    <div class="chat">
+      <div class="chat-title">
+        <!-- Chatbot Title and Status -->
+        <div>
+          <h1>PyBot</h1>
+          <h2>ACTIVE</h2>
+        </div>
+        <figure class="avatar">
+          <img src="{{ asset('admin-assets/media/avatar/5.jpg') }}" />
+        </figure>
+        <span class="chat-box-toggle"><i class="bi bi-x"></i></span>
       </div>
-      <figure class="avatar">
-        <img src="{{ asset('admin-assets/media/avatar/5.jpg') }}" />
-      </figure>
-      <span class="chat-box-toggle"><i class="bi bi-x"></i></span>
+      <div class="messages">
+        <div class="messages-content"></div>
+          <!-- Chatbot messages go here -->
+
+      </div>
+      
+      <div class="message-box">
+        <textarea type="text" class="message-input" placeholder="Type message..."></textarea>
+        <button type="submit" class="message-submit">Send</button>
+      </div>
     </div>
-    <div class="messages">
-      <div class="messages-content"></div>
-    </div>
-    <div class="message-box">
-      <textarea type="text" class="message-input" placeholder="Type message..."></textarea>
-      <button type="submit" class="message-submit">Send</button>
-    </div>
-  </div>
+
 
 
   <main id="home">
