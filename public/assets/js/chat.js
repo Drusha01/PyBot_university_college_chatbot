e2046fbd0572e08fc465557af621344f710cfd30
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2023-10-23 12:35:07 +0800

    Revert "chat js"
    This reverts commit 975e679ced34d8c309f35a51cd19200a7e33f606.

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -17,11 +17,7 @@ function setDate(){
   
 }
 
-$(".message-input").keyup(function(event) {
-  if (event.keyCode === 13) {
-      $('.message-submit').click();
-  }
-});
+
 
 $('.message-submit').click(function() {
   msg = $('.message-input').val();
