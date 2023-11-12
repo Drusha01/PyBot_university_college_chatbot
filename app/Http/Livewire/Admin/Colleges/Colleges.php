c8b4c45f49fc514beef16928a01d69adedd76ed4
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2023-11-13 01:07:35 +0800

    today

--- app/Http/Livewire/Admin/Colleges/Colleges.php
+++ app/Http/Livewire/Admin/Colleges/Colleges.php
@@ -14,22 +14,35 @@ class Colleges extends Component
 
     public $ccs_q_and_a_data;
     public $ccs_q_and_a_filter;
+    public $active;
 
+    public $q_and_a = [
+        'q_and_a_id' => NULL,
+        'questions'=>NULL,
+        'answers'=>NULL,
+        'q_and_a_tag'=>NULL,
+        'q_and_a_type_id'=>NULL,
+        'q_and_a_target_type_id'=>NULL
+    ];
+    public $question = [
+        "question_id" => NULL,
+        "question_q_and_a_id"=> NULL,
+        "question_details"=> NULL,
+    ];
 
-    public function mount(){
-        $this->title = 'Colleges';
+    public $target_type_id;
+    public $target_types = [];
 
-        $this->ccs_q_and_a_filter = [
-            '#' => true,
-            'Questions' => true,
-            'Tags'=> true,
-            'Responses'=> true,
-            'Actions'	=> true
-        ];
+    public function hydrate(){
+        self::update_data();
+    }
+    public function update_data(){
         $this->ccs_data = DB::table('q_and_a as qa')
-        ->select('q_and_a_id','q_and_a_tag')
+        ->select('q_and_a_id','q_and_a_tag','target_type_details','target_type_id')
         ->join('q_and_a_types as qt', 'qt.q_and_a_type_id', '=', 'qa.q_and_a_type_id')
+        ->join('target_types as tt', 'tt.target_type_id', '=', 'qa.q_and_a_target_type_id')
         ->where('qt.q_and_a_type_details','=','CCS')
+        ->orderBy('q_and_a_id')
         ->get()
         ->toArray();
         $this->ccs_q_and_a_data = [];
@@ -53,19 +66,433 @@ class Colleges extends Component
             foreach ($answers as $key => $a_value) {
                 array_push($answers_data,$a_value->answer_details);
             }
-            array_push($this->ccs_q_and_a_data,
-            ['questions'=>$questions_data,
-            'tags'=>$value->q_and_a_tag,
-            'answers'=>$answers_data]);
+            
+            array_push($this->ccs_q_and_a_data,[
+                'questions'=>$questions_data,
+                'tags'=>$value->q_and_a_tag,
+                'target_type_details'=>$value->target_type_details,
+                'target_type_id'=>$value->target_type_id,
+                'answers'=>$answers_data,
+                'q_and_a_id' =>$value->q_and_a_id 
+            ]);
         }
+
+
+        $this->csc_data = DB::table('q_and_a as qa')
+        ->select('q_and_a_id','q_and_a_tag','target_type_details','target_type_id')
+        ->join('q_and_a_types as qt', 'qt.q_and_a_type_id', '=', 'qa.q_and_a_type_id')
+        ->join('target_types as tt', 'tt.target_type_id', '=', 'qa.q_and_a_target_type_id')
+        ->where('qt.q_and_a_type_details','=','CSC')
+        ->orderBy('q_and_a_id')
+        ->get()
+        ->toArray();
+        $this->csc_q_and_a_data = [];
+        foreach ($this->csc_data as $key => $value) {
+            $questions = DB::table('questions as q')
+                ->where('question_q_and_a_id','=',$value->q_and_a_id)
+                ->get()
+                ->toArray();
+
+            $questions_data =[];
+            foreach ($questions as $key => $q_value) {
+                array_push($questions_data,$q_value->question_details);
+            }
+
+            $answers = DB::table('answers as a')
+                ->where('answer_q_and_a_id','=',$value->q_and_a_id)
+                ->get()
+                ->toArray();
+
+            $answers_data =[];
+            foreach ($answers as $key => $a_value) {
+                array_push($answers_data,$a_value->answer_details);
+            }
+            
+            array_push($this->csc_q_and_a_data,[
+                'questions'=>$questions_data,
+                'tags'=>$value->q_and_a_tag,
+                'target_type_details'=>$value->target_type_details,
+                'target_type_id'=>$value->target_type_id,
+                'answers'=>$answers_data,
+                'q_and_a_id' =>$value->q_and_a_id 
+            ]);
+        }
+
+        $this->target_types = DB::table('target_types')
+            ->get()
+            ->toArray();
+    }
+
+    public function mount(){
+        $this->title = 'Colleges';
+        $this->active = 'college_management';
+
+        $this->ccs_q_and_a_filter = [
+            '#' => true,
+            'Questions' => true,
+            'Tags'=> true,
+            'Target'=> true,
+            'Responses'=> true,
+            'Actions'	=> true
+        ];
+        $this->csc_q_and_a_filter = [
+            '#' => true,
+            'Questions' => true,
+            'Tags'=> true,
+            'Target'=> true,
+            'Responses'=> true,
+            'Actions'	=> true
+        ];
+        self::update_data();
         // dd($this->ccs_q_and_a_data);
     }
 
-    public function render()
-    {
+    public function render(){
         return view('livewire.admin.colleges.colleges',[
             ])
             ->layout('layouts.admin',[
                 'title'=>$this->title]);
+    }
+    public function active_page($active){
+        $this->active = $active;
+    }
+    public function edit_q_and_a($q_and_a_id){
+        $this->access_role = [
+            'C' => true,
+            'R' => true,
+            'U' => true,
+            'D' => true
+        ];
+        if($this->access_role['C']  ){
+            $q_and_a = DB::table('q_and_a as qa')
+                ->join('q_and_a_types as qt', 'qt.q_and_a_type_id', '=', 'qa.q_and_a_type_id')
+                ->join('target_types as tt', 'tt.target_type_id', '=', 'qa.q_and_a_target_type_id')
+                ->where('q_and_a_id','=',$q_and_a_id)
+                ->first();
+                $this->target_type_id = $q_and_a->target_type_id;
+
+            $questions = DB::table('questions')
+                ->where('question_q_and_a_id','=',$q_and_a_id)
+                ->get()
+                ->toArray();
+            $answers = DB::table('answers')
+            ->where('answer_q_and_a_id','=',$q_and_a_id)
+            ->get()
+            ->toArray();
+
+            $questions_list = [];
+            foreach ($questions as $key => $value) {
+                array_push($questions_list,[
+                    'question_details'=>$value->question_details,
+                    'question_id'=>$value->question_id
+                ]);
+            }
+
+            $answers_list = [];
+            foreach ($answers as $key => $value) {
+                array_push($answers_list,[
+                    'answer_details'=>$value->answer_details,
+                    'answer_id'=>$value->answer_id
+                ]);
+            }
+           
+
+            $this->q_and_a = [
+                'q_and_a_id' => $q_and_a_id,
+                'questions'=>$questions_list,
+                'answers'=>$answers_list,
+                'q_and_a_tag'=>NULL,
+                'q_and_a_type_id' => $q_and_a->q_and_a_type_id,
+                'q_and_a_type_details'=>$q_and_a->q_and_a_type_details,
+                'target_type_id'=> $q_and_a->target_type_id,
+                'target_type_details'=>$q_and_a->target_type_details
+            ];
+            $this->dispatchBrowserEvent('openModal','editCollegeModal');
+        }
+    }
+    public function add_q_and_a(){
+        $questions_list = [];
+        array_push($questions_list,[
+            'question_details'=>NULL,
+            'question_id'=>NULL
+        ]);
+    
+
+        $answers_list = [];
+    
+        array_push($answers_list,[
+            'answer_details'=>NULL,
+            'answer_id'=>NULL
+        ]);
+
+        $target_type_id = NULL;
+        if($this->target_types){
+            foreach ($this->target_types as $key => $value) {
+                $target_type_id = $value->target_type_id;
+                break;
+            }
+        }
+
+        $q_and_a_type_id =DB::table('q_and_a_types')
+            ->where('q_and_a_type_details','=','CCS')
+            ->first()->q_and_a_type_id;
+
+        $this->q_and_a = [
+            'q_and_a_id' => 0,
+            'questions'=>$questions_list,
+            'answers'=>$answers_list,
+            'q_and_a_tag'=>NULL,
+            'q_and_a_type_id' => $q_and_a_type_id,
+            'q_and_a_type_details'=>NULL,
+            'target_type_id'=> $target_type_id,
+            'target_type_details'=>NULL
+        ];
+
+        $this->dispatchBrowserEvent('openModal','AddCollegeModal');
+    }
+    public function save_add_q_and_a(){
+        if(isset($this->q_and_a['q_and_a_id'])){
+            $questions = $this->q_and_a['questions'];
+            $question_valid = false;
+            foreach ($questions as $key => $value) {
+                if(strlen($value['question_details'])>0){
+                    $question_valid = true;
+                    break;
+                }
+            }
+
+            $answer_valid = false;
+            $answers = $this->q_and_a['answers'];
+            foreach ($answers as $key => $value) {
+                if(strlen($value['answer_details'])>0){
+                    $answer_valid = true;
+                    break;
+                }
+            }
+            if($question_valid && $answer_valid ){
+               DB::table('q_and_a')
+                ->insert([
+                    'q_and_a_type_id'=>$this->q_and_a['q_and_a_type_id'],
+                    'q_and_a_tag'=>NULL,
+                    'q_and_a_target_type_id'=>$this->q_and_a['target_type_id']
+                ]);
+                $q_and_a = DB::table('q_and_a as qa')
+                    ->orderBy('q_and_a_id','desc')
+                    ->first();
+
+                foreach ($questions as $key => $value) {
+                    if(strlen($value['question_details'])>0){
+                        DB::table('questions')
+                        ->insert([
+                            'question_details'=> $value['question_details'],
+                            'question_q_and_a_id'=>$q_and_a->q_and_a_id
+                    ]);
+                    }
+                }
+                foreach ($answers as $key => $value) {
+                    if(strlen($value['answer_details'])>0){
+                        DB::table('answers')
+                        ->insert([
+                            'answer_details'=> $value['answer_details'],
+                            'answer_q_and_a_id'=>$q_and_a->q_and_a_id
+                    ]);
+                    }
+                }
+                self::update_data();
+           
+                $this->dispatchBrowserEvent('openModal','AddCollegeModal');
+                $this->dispatchBrowserEvent('swal:redirect',[
+                    'position'          									=> 'center',
+                    'icon'              									=> 'success',
+                    'title'             									=> 'successfully added!',
+                    'showConfirmButton' 									=> 'true',
+                    'timer'             									=> '1000',
+                    'link'              									=> '#'
+                ]);
+
+            }else{
+                if(!$answer_valid){
+                    $this->dispatchBrowserEvent('swal:redirect',[
+                        'position'          									=> 'center',
+                        'icon'              									=> 'warning',
+                        'title'             									=> 'Please provide a valid answer!',
+                        'showConfirmButton' 									=> 'true',
+                        'timer'             									=> '1000',
+                        'link'              									=> '#'
+                    ]);
+                }
+                if(!$question_valid){
+                    $this->dispatchBrowserEvent('swal:redirect',[
+                        'position'          									=> 'center',
+                        'icon'              									=> 'warning',
+                        'title'             									=> 'Please provide a valid question!',
+                        'showConfirmButton' 									=> 'true',
+                        'timer'             									=> '1000',
+                        'link'              									=> '#'
+                    ]);
+                }
+                
+            }
+        }
+        
+    }
+    public function save_edit_q_and_a($q_and_a_id){
+        if(intval($q_and_a_id)>0 && $q_and_a = DB::table('q_and_a as qa')
+            ->where('q_and_a_id','=',$q_and_a_id)
+            ->first()){
+            $valid = true;
+            $questions = $this->q_and_a['questions'];
+            DB::table('questions')
+                ->where('question_q_and_a_id','=',$q_and_a_id)
+                ->delete();
+            foreach ($questions as $key => $value) {
+                if(strlen($value['question_details'])>0){
+                    DB::table('questions')
+                        ->insert([
+                            'question_details'=> $value['question_details'],
+                            'question_q_and_a_id'=>$q_and_a_id
+                    ]);
+                }
+            }
+            $answers = $this->q_and_a['answers'];
+            DB::table('answers')
+                ->where('answer_q_and_a_id','=',$q_and_a_id)
+                ->delete();
+            foreach ($answers as $key => $value) {
+                if(strlen($value['answer_details'])>0){
+                    DB::table('answers')
+                        ->insert([
+                            'answer_details'=> $value['answer_details'],
+                            'answer_q_and_a_id'=>$q_and_a_id
+                    ]);
+                }
+            }
+            DB::table('q_and_a')
+                ->where('q_and_a_id','=',$q_and_a_id)
+                ->update(['q_and_a_target_type_id'=>$this->q_and_a['target_type_id']]);
+
+            self::update_data();
+           
+            $this->dispatchBrowserEvent('openModal','editCollegeModal');
+            $this->dispatchBrowserEvent('swal:redirect',[
+                'position'          									=> 'center',
+                'icon'              									=> 'success',
+                'title'             									=> 'successfully updated!',
+                'showConfirmButton' 									=> 'true',
+                'timer'             									=> '1000',
+                'link'              									=> '#'
+            ]);
+        }
+    }
+    public function add_question(){
+        array_push($this->q_and_a['questions'],[
+            'question_details'=>NULL,
+            'question_id'=>NULL
+        ]);
+    }
+    public function add_answer(){
+        array_push($this->q_and_a['answers'],[
+            'answer_details'=>NULL,
+            'answer_id'=>NULL
+        ]);
+    }
+    public function remove_question($key){
+        $questions = $this->q_and_a['questions'];
+        $questions_list = [];
+       foreach ($questions as $q_key => $value) {
+            if($key != $q_key){
+                array_push($questions_list,$value);
+            }
+       }
+       $this->q_and_a['questions'] = $questions_list;
+    }
+    public function remove_answer($key){
+        $answers = $this->q_and_a['answers'];
+        $answers_list = [];
+       foreach ($answers as $q_key => $value) {
+            if($key != $q_key){
+                array_push($answers_list,$value);
+            }
+       }
+       $this->q_and_a['answers'] = $answers_list;
+    }
+    public function delete_q_and_a($q_and_a_id){
+        $this->access_role = [
+            'C' => true,
+            'R' => true,
+            'U' => true,
+            'D' => true
+        ];
+        if($this->access_role['D']  ){
+
+            $q_and_a = DB::table('q_and_a as qa')
+                ->join('q_and_a_types as qt', 'qt.q_and_a_type_id', '=', 'qa.q_and_a_type_id')
+                ->join('target_types as tt', 'tt.target_type_id', '=', 'qa.q_and_a_target_type_id')
+                ->where('q_and_a_id','=',$q_and_a_id)
+                ->first();
+                $this->target_type_id = $q_and_a->target_type_id;
+
+            $questions = DB::table('questions')
+                ->where('question_q_and_a_id','=',$q_and_a_id)
+                ->get()
+                ->toArray();
+            $answers = DB::table('answers')
+            ->where('answer_q_and_a_id','=',$q_and_a_id)
+            ->get()
+            ->toArray();
+
+            $questions_list = [];
+            foreach ($questions as $key => $value) {
+                array_push($questions_list,[
+                    'question_details'=>$value->question_details,
+                    'question_id'=>$value->question_id
+                ]);
+            }
+
+            $answers_list = [];
+            foreach ($answers as $key => $value) {
+                array_push($answers_list,[
+                    'answer_details'=>$value->answer_details,
+                    'answer_id'=>$value->answer_id
+                ]);
+            }
+           
+            $this->q_and_a = [
+                'q_and_a_id' => $q_and_a_id,
+                'questions'=>$questions_list,
+                'answers'=>$answers_list,
+                'q_and_a_tag'=>NULL,
+                'q_and_a_type_id' => $q_and_a->q_and_a_type_id,
+                'q_and_a_type_details'=>$q_and_a->q_and_a_type_details,
+                'target_type_id'=> $q_and_a->target_type_id,
+                'target_type_details'=>$q_and_a->target_type_details
+            ];
+            $this->dispatchBrowserEvent('openModal','DeletecollegeModal');
+        }
+    }
+    public function save_delete_q_and_a($q_and_a_id){
+        DB::table('questions')
+            ->where('question_q_and_a_id','=',$q_and_a_id)
+            ->delete();
+
+            DB::table('answers')
+            ->where('answer_q_and_a_id','=',$q_and_a_id)
+            ->delete();
+        
+        DB::table('q_and_a as qa')
+            ->where('q_and_a_id','=',$q_and_a_id)
+            ->delete();
+
+        self::update_data();
+        
+        $this->dispatchBrowserEvent('openModal','DeletecollegeModal');
+        $this->dispatchBrowserEvent('swal:redirect',[
+            'position'          									=> 'center',
+            'icon'              									=> 'success',
+            'title'             									=> 'successfully updated!',
+            'showConfirmButton' 									=> 'true',
+            'timer'             									=> '1000',
+            'link'              									=> '#'
+        ]);
     }
 }
---
+++ resources/views/livewire/components/navigation/admin-navigation.blade.php
@@ -0,0 +1,169 @@
+<div>
+
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center pl-10">
+            <a class="navbar-brand brand-logo mr-5" href="{{ route('dashboard') }}">
+                <img src="{{ asset('admin-assets/media/pybot_icon.svg') }}" class="mr-2" alt="logo">
+                <img src="{{ asset('admin-assets/media/logo.svg') }}" alt="name">
+            </a>
+            <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}">
+                <img src="{{ asset('admin-assets/media/pybot_icon.svg') }}" alt="logo">
+            </a>
+        </div>
+
+        <div class="navbar-menu-wrapper d-flex align-items-center justify-content-between">
+            
+            <div class="search-input">
+                <a href="" target="_blank" hidden></a>
+                <input type="text" placeholder="Type to search..">
+                <div class="autocom-box">
+                </div>
+                <div class="icon"><i class="bi bi-search"></i></div>
+            </div>
+
+            <ul class="navbar-nav navbar-nav-right bs-borderbox">
+
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" data-toggle="dropdown">
+                        <i class='bi bi-bell mx-0'></i>
+                        <span class="count"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                        <div>
+                            <p class="mb-0 font-weight-normal text-center float-left dropdown-header border-bottom">You have 4 new notifications</p>
+                        </div>
+                        <div>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-success">
+                                        <i class="bi bi-info-circle-fill mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">Application Error</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted">
+                                        Just now
+                                    </p>
+                                </div>
+                            </a>
+                            <div class="border-top text-center">
+                                <a href="{{route ('admin-notification')}}"><span class="p-2 ms-2">View all</span></a></p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                    <li class="nav-item nav-profile dropdown text-dark px-2">
+                        <div>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="profileDropdown">
+                                <img src="{{ asset('admin-assets\media\avatar\6.jpg') }}" alt="profile"/>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                                <a class="dropdown-item" href="{{route ('admin-profile') }}">
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="{{route ('setting')}}">
+                                    Settings
+                                </a>
+                                <a class="dropdown-item" href="{{route('logout')}}">
+                                    Logout
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="font-weight-normal mb-0">Admin Pybot</h6>
+                        </div>
+                    </li>
+                </li>
+            </ul>
+
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                <span class="bi bi-list"></span>
+            </button>
+        </div>
+    </nav>
+    <script>
+        // Toggle sidebar visibility
+        $('[data-toggle="minimize"]').on("click", function () {
+        if (body.hasClass('sidebar-toggle-display') || body.hasClass('sidebar-absolute')) {
+            body.toggleClass('sidebar-hidden');
+        } else {
+            body.toggleClass('sidebar-icon-only');
+        }
+        });
+        
+    let suggestions = [
+        "Dashboard",
+        "Colleges",
+        "Departments",
+        "User Management",
+        "Chatbox",
+        "Settings",
+        "Model",
+        "College Student Council (CSC)",
+        "Admin Profile",
+        "Admin Notification",
+        "Notifications",
+        "Profiles",
+        "Models",
+        "Intents Creation",
+        "Training",
+        "Deployment",
+    ];
+    // getting all required elements
+    const searchWrapper = document.querySelector(".search-input");
+    const inputBox = searchWrapper.querySelector("input");
+    const suggBox = searchWrapper.querySelector(".autocom-box");
+    const icon = searchWrapper.querySelector(".icon");
+    let linkTag = searchWrapper.querySelector("a");
+    let webLink;
+    // if user press any key and release
+    inputBox.onkeyup = (e)=>{
+        let userData = e.target.value; //user enetered data
+        let emptyArray = [];
+        if(userData){
+            icon.onclick = ()=>{
+                webLink = `https://www.google.com/search?q=${userData}`;
+                linkTag.setAttribute("href", webLink);
+                linkTag.click();
+            }
+            emptyArray = suggestions.filter((data)=>{
+                //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+                return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+            });
+            emptyArray = emptyArray.map((data)=>{
+                // passing return data inside li tag
+                return data = `<li>${data}</li>`;
+            });
+            searchWrapper.classList.add("active"); //show autocomplete box
+            showSuggestions(emptyArray);
+            let allList = suggBox.querySelectorAll("li");
+            for (let i = 0; i < allList.length; i++) {
+                //adding onclick attribute in all li tag
+                allList[i].setAttribute("onclick", "select(this)");
+            }
+        }else{
+            searchWrapper.classList.remove("active"); //hide autocomplete box
+        }
+    }
+    function select(element){
+        let selectData = element.textContent;
+        inputBox.value = selectData;
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${selectData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        searchWrapper.classList.remove("active");
+    }
+    function showSuggestions(list){
+        let listData;
+        if(!list.length){
+            userValue = inputBox.value;
+            listData = `<li>${userValue}</li>`;
+        }else{
+        listData = list.join('');
+        }
+        suggBox.innerHTML = listData;
+    }
+    </script>
+</div>
