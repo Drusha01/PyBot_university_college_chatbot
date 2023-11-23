1644a16f6ee9358974408b8a327f238c96bc070a
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2023-11-23 22:55:55 +0800

    crud

--- resources/views/livewire/page/home/home.blade.php
+++ resources/views/livewire/page/home/home.blade.php
@@ -1,6 +1,44 @@
 <div>
   <main class="content-wrapper" id="home">
-    @if($carousel_data || 1)
+    @if($carousel_data)
+      <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          @foreach ($carousel_data as $key =>$value)
+            @if($key == 0)
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide {{ $key+1}}"></button>
+            @else
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide {{ $key+1}}"></button>
+            @endif
+          @endforeach
+        </div>
+      <div class="carousel-inner">
+      @foreach ($carousel_data as $key =>$value)
+        @if($key == 0)
+          <div class="carousel-item active" >
+            <img src="{{asset('storage/content/carousel/'.$value->carousel_content_image)}}" class="card-img-top mx-auto" alt="Admission Image {{ $key+1}}">
+            <div class="banner"></div>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1 class="animated fadeInDown" style="text-shadow: 2px 4px 8px #0b4737;">{{$value->carousel_header_title}}</h1>
+                <p class="animated fadeInUp" style="-webkit-text-stroke: 0.9px #b1beb9; text-shadow: 2px 4px 8px #0b4737;">{{$value->carousel_paragraph_paragraph}}</p>
+                <p class="animated fadeInUp"><a class="btn btn-md btn-success background-color-green btn-apply d-none" href="#">Learn More</a></p>
+              </div>
+            </div>
+          </div>
+        @else
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide {{ $key+1}}"></button>
+        @endif
+      @endforeach
+      <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden ">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden ">Next</span>
+      </button>
+      </div>
+    @else
     <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -52,8 +90,44 @@
       </button>
     </div>
     @endif
-    
-    <!-- <div class="container marketing">
+
+    @if(0 && $academic_data)
+      <div class="container marketing">
+        <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="academics">
+          <h1 class="display-4 fw-normal">Programs</h1>
+          <p class="fs-5">Our programs are always up-to-date, following the CHED recommendation, industry standards,
+            and feedback from our successful alumni to ensure we always deliver quality education and produce highly employable
+            graduates.
+          </p>
+        </div>
+        <hr class="featurette-divider">
+      @foreach($academic_data as $key=>$value)
+        <div class="row featurette featurette-1">
+          <div class="col-12 col-md-12 col-lg-7 course-cs">
+            <h2 class="featurette-heading course-title">BS in Computer Science</span></h2>
+            <p class="lead justify">
+              The computer science course is intended for students with a solid background in mathematics
+              and an interest in the theory, practice, art, and science of computer programming. This course provides
+              graduates with an educational foundation for careers in computer software or computer system design,
+              including careers as software engineers, application software designers, system programmers, or system engineers.
+            </p>
+            <p class="btn-apply-center"><a class="btn btn-md btn-success background-color-green btn-apply btn-font" href="../student/apply.php?program=bscs">Apply Now</a></p>
+          </div>
+          <div class="col-12 col-md-10 col-lg-5 mx-auto mx-lg-0 course-1"></div>
+        </div>
+        
+        <div class="col-md-3">
+          <div class="card mb-4">
+            <img src="{{asset('storage/content/academic/'.$value->academic_logo)}}" class="card-img-top " alt="Program Image">
+            <div class="card-body">
+              <h5 class="card-title">{{$value->academic_header}}</h5>
+              <p class="card-text cqh-7">{{$value->academic_content}}</p>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    @else
+    <div class="container marketing">
       <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="academics">
         <h1 class="display-4 fw-normal">Programs</h1>
         <p class="fs-5">Our programs are always up-to-date, following the CHED recommendation, industry standards,
@@ -88,7 +162,31 @@
         </div>
         <div class="col-12 col-md-10 col-lg-5 mx-auto mx-lg-0 course-1 course-2"></div>
       </div>
+      @endif
       <hr class="featurette-divider">
+
+      @if($academic_data)
+          <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="admissions">
+            <h1 class="display-4 fw-normal">Admissions</h1>
+            <p class="fs-5">
+            Our admission process is now powered by a decision-support system called - <span class="green-bold fs-5">Pybot</span>.
+            </p>
+            <p></p>
+          </div>
+          <div class="row justify-content-xl-center">
+          @foreach($academic_data as $key=>$value)
+            <div class="col-md-3">
+              <div class="card mb-4">
+                <img src="{{asset('storage/content/academic/'.$value->academic_logo)}}" class="card-img-top " alt="Program Image">
+                <div class="card-body">
+                  <h5 class="card-title">{{$value->academic_header}}</h5>
+                  <p class="card-text cqh-7">{{$value->academic_content}}</p>
+                </div>
+              </div>
+            </div>
+          @endforeach
+          </div>
+      @else
       <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="admissions">
         <h1 class="display-4 fw-normal">Admissions</h1>
         <p class="fs-5">
@@ -96,7 +194,28 @@
         </p>
         <p><a class="btn btn-md btn-success background-color-green text-center btn-apply btn-font" href="../student/apply.php">Apply Now</a></p>
       </div>
+      @endif
+
       <hr class="featurette-divider">
+      @if($faculty_data)
+        <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="faculty">
+          <h1 class="display-4 fw-normal">Faculty and Staff</h1>
+        </div>
+        <div class="row faculty justify-content-xl-center">
+        @foreach($faculty_data as $key =>$value)
+          <div class="col-lg-4">
+            <div class="row">
+              <div class="col-12 mb-2">
+              <img src="{{asset('storage/content/faculty/'.$value->faculty_picture)}}" alt="" class="bd-placeholder-img rounded w-75">
+              </div>
+              <div class="col-12 border-top pt-3">
+              <h3>{{$value->faculty_fullname}}</h3>
+              <h4>{{$value->faculty_position}}</h4>
+              </div>
+            </div>
+          </div>
+        @endforeach
+      @else
       <div class="pricing-header pt-md-4 pb-md-4 mx-auto text-center" id="faculty">
         <h1 class="display-4 fw-normal">Faculty and Staff</h1>
       </div>
@@ -139,8 +258,9 @@
           <h4>IT Department Head</h4>
         </div>
       </div>
+      @endif
       <hr class="featurette-divider">
-    </div> -->
+    </div>
   </main>
 </div>
 
