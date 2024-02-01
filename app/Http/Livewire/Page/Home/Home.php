fa87f677a84e7604d3b88911342197cd8906bebe
Drusha02 <hanz.dumapit54@gmail.com>
2024-02-01 23:07:44 +0800

    update

--- app/Http/Livewire/Page/Home/Home.php
+++ app/Http/Livewire/Page/Home/Home.php
@@ -11,27 +11,6 @@ use Illuminate\Support\Facades\Storage;
 class Home extends Component
 {
     public function booted(Request $request){
-        $this->user_details = $request->session()->all();
-        if(!isset($this->user_details['user_id'])){
-            header("Location: /login");
-            die();
-        }else{
-            $user_status = DB::table('users as u')
-            ->select('u.user_status_id','us.user_status_details')
-            ->join('user_status as us', 'u.user_status_id', '=', 'us.user_status_id')
-            ->where('user_id','=', $this->user_details['user_id'])
-            ->first();
-        }
-
-        if(isset($user_status->user_status_details) && $user_status->user_status_details == 'deleted' ){
-            header("Location: /deleted");
-            die();
-        }
-
-        if(isset($user_status->user_status_details) && $user_status->user_status_details == 'inactive' ){
-            header("Location: /inactive");
-            die();
-        }
     }
     public function hydrate(){
         
